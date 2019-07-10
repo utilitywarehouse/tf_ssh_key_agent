ddd9ad4c2e5a1e570cedba09c70092713f0f044b
Rob Best <robertbest89@gmail.com>
2019-07-10 08:33:00 +0100

    Initial commit of module code

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "uri" {
+  type        = "string"
+  description = "The S3 URI of the authmap file"
+}
+
+variable "groups" {
+  type        = "list"
+  description = "A list of allowed google groups"
+}
+
+variable "ssh_key_agent_version" {
+  type        = "string"
+  description = "The ssh-key-agent version"
+}
